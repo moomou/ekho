9df06963445d96bbb587544d9e5e3ed877d1d235
paul <ppymou@gmail.com>
2014-04-12 09:52:20 -0700

    Tmp

--- server/src/routes/embed.js
+++ server/src/routes/embed.js
@@ -1,9 +1,23 @@
 var util = require('./util'),
-    handlebars = require('handlebars');
+    handlebars = require('handlebars'),
+    fs = require('fs');
 
 bootstrapTemplate = handlebars.compile(
-    fs.readFileSync("__dirname/../templates/bootstrap.js.handlebars").toString())
+    fs.readFileSync(__dirname + "/../templates/bootstrap.js.handlebars").toString())
 
 exports.bootstrap = function(req, res, next) {
-    var ui = req.query.ui;
+    function randomId() {
+        return "" + Math.floor(Math.random() * 167772159999999).toString(16);
+    }
+
+    var jsFile =  bootstrapTemplate({
+        ENABLE_UI  : req.query.ui,
+        SCRIPT_ID  : req.query.scriptId || randomId(),
+        DIV_ID     : req.query.divId || randomId(),
+        RENDERED   : '', // to change to UI later
+        SERVER_URL : 'http://localhost:3000/'
+    });
+
+    res.setHeader("Content-Type", "text/javascript");
+    return res.status(200).end(jsFile);
 };
