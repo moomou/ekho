fd6f9f555af29eeb01d72cdca4b0b4e26bce90c5
paul <ppymou@gmail.com>
2014-04-14 21:53:02 -0700

    container fix

--- server/src/routes/embed.js
+++ server/src/routes/embed.js
@@ -11,11 +11,12 @@ exports.bootstrap = function(req, res, next) {
     }
 
     var jsFile =  bootstrapTemplate({
-        ENABLE_UI  : req.query.ui,
-        SCRIPT_ID  : req.query.scriptId || randomId(),
-        DIV_ID     : req.query.divId || randomId(),
-        RENDERED   : '<div id="ekho-container"> <form action="" id="search"> <div> <label for="name">Text Input:</label> <div id="info-field"> <input type="text" name="name" id="info-input" class="attention" value="" tabindex="1" placeholder="Search..."/> </div> <div class="effect-copy attention"></div> <div id="button-field"> <input type="submit" id="button-input" class="attention" value="Submit" /> </div> </div> </form> </div>',
-        SERVER_URL : 'http://54.186.177.249/'
+        ENABLE_UI    : req.query.ui,
+        SCRIPT_ID    : req.query.scriptId || randomId(),
+        DIV_ID       : req.query.divId || randomId(),
+        CONTAINER_ID : req.query.containerId || '',
+        RENDERED     : '<div id="ekho-container"> <form action="" id="search"> <div> <label for="name">Text Input : </label> <div id="info-field"> <input type="text" name="name" id="info-input" class="attention" value="" tabindex="1" placeholder="Search..."/> </div> <div class="effect-copy attention"></div> <div id="button-field"> <input type="submit" id="button-input" class="attention" value="Submit" /> </div> </div> </form> </div>',
+        SERVER_URL   : 'http                                                                                      : //54.186.177.249/'
     });
 
     res.setHeader("Content-Type", "text/javascript");
